199221dd8462f36d3217ff24c646eb1e1fe187a5
roy-hendry <royjairusraj@hotmail.com>
2022-10-19 15:19:13 +0100

    Error & Exception Handling

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddlware");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+// Allows us to send raw json
+app.use(express.json());
+// Allows us to accept the url encoded form
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
 	res.status(200).json({ message: "Welcome to the Support Desk API" });
@@ -10,6 +16,8 @@ app.get("/", (req, res) => {
 
 // Routes
 // /api/users has been set to work on the userRoutes file's contents
-app.use("/api/users", require("./routes/userRoutes"))
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
