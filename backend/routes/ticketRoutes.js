583cc8afaf5c2a15391b5856441d8435e20d4421
roy-hendry <royjairusraj@hotmail.com>
2022-11-05 16:19:47 +0000

    Single Ticket, Update & Delete (Backend)

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const { getTickets, createTicket } = require("../controllers/ticketController");
+const {
+	getTickets,
+	getTicket,
+	createTicket,
+	deleteTicket,
+	updateTicket,
+} = require("../controllers/ticketController");
 
 const { protect } = require("../middleware/authMiddleware");
 
 // By using route("/") we can stack multiple types of requests onto the route. This way we don't need to do get and post requests seperately
 router.route("/").get(protect, getTickets).post(protect, createTicket);
+
+// Now when we are on the route for the ticket id we can perform these seperare requests
+router
+	.route("/:id")
+	.get(protect, getTicket)
+	.delete(protect, deleteTicket)
+	.put(protect, updateTicket);
 
 module.exports = router;
