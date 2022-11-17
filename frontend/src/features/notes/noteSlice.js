47e6fd5e49f5e44005e0bb73761012f48c98fd89
roy-hendry <royjairusraj@hotmail.com>
2022-11-17 18:03:47 +0000

    Submit a Note

--- frontend/src/features/notes/noteSlice.js
+++ frontend/src/features/notes/noteSlice.js
@@ -9,13 +9,33 @@ const initialState = {
 	message: "",
 };
 
-// Get a ticket notes
+// Get a ticket's notes
 export const getNotes = createAsyncThunk(
 	"notes/getAll",
 	async (ticketId, thunkAPI) => {
 		const token = thunkAPI.getState().auth.user.token;
 		try {
 			return await noteService.getNotes(ticketId, token);
+		} catch (error) {
+			const message =
+				(error.response &&
+					error.response.data &&
+					error.response.data.message) ||
+				error.message ||
+				error.toString();
+
+			return thunkAPI.rejectWithValue(message);
+		}
+	}
+);
+
+// Create ticket note
+export const createNote = createAsyncThunk(
+	"notes/create",
+	async ({noteText, ticketId}, thunkAPI) => {
+		const token = thunkAPI.getState().auth.user.token;
+		try {
+			return await noteService.createNote(noteText, ticketId, token);
 		} catch (error) {
 			const message =
 				(error.response &&
@@ -46,6 +66,20 @@ export const noteSlice = createSlice({
 				state.notes = action.payload;
 			})
 			.addCase(getNotes.rejected, (state, action) => {
+				state.isLoading = false;
+				state.isError = true;
+				state.message = action.payload;
+			})
+			.addCase(createNote.pending, (state) => {
+				state.isLoading = true;
+			})
+			.addCase(createNote.fulfilled, (state, action) => {
+				state.isLoading = false;
+				state.isSuccess = true;
+				// This is normally not possible with regular redux as it is usually immutable, however with redux toolkit we can do this
+				state.notes.push(action.payload)
+			})
+			.addCase(createNote.rejected, (state, action) => {
 				state.isLoading = false;
 				state.isError = true;
 				state.message = action.payload;
--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -4,7 +4,11 @@ import Modal from "react-modal";
 import { FaPlus } from "react-icons/fa";
 import { useSelector, useDispatch } from "react-redux";
 import { getTicket, reset, closeTicket } from "../features/tickets/ticketSlice";
-import { getNotes, reset as notesReset } from "../features/notes/noteSlice";
+import {
+	getNotes,
+	createNote,
+	reset as notesReset,
+} from "../features/notes/noteSlice";
 import { useParams, useNavigate } from "react-router-dom";
 import BackButton from "../components/BackButton";
 import Spinner from "../components/Spinner";
@@ -63,7 +67,7 @@ function Ticket() {
 	// Create note submit
 	const onNoteSubmit = (e) => {
 		e.preventDefault();
-		console.log("Submit");
+		dispatch(createNote({ noteText, ticketId }));
 		closeModal();
 	};
 
